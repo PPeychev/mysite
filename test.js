05b8f22b232c93972b756ab299e4a7ece40b3746
Plamen Peychev <peychevbg@gmail.com>
2025-09-15 18:13:10 +0300

    change button text
    Added function changeButtonText()

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
+see = false;
+
+function changeButtonText(){
+    if (see == true)
+        document.getElementById("btn").innerHTML="Show text";
+    else
+        document.getElementById("btn").innerHTML="Hide text";  
+}
+
 function myFunction(){
-    document.getElementById('demo').style.display='block';
+    
+    if (see == false){
+        document.getElementById('demo').style.display='block';
+        changeButtonText()
+        see = true;
+    }
+    else{
+        document.getElementById('demo').style.display='none';
+        changeButtonText()
+        see = false;
+    }
+        
 }
